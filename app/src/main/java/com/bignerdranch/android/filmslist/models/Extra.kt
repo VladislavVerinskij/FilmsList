64677e55d2ad2acc2a7a2ce20c8b2db99589d39d
Vladislav <vladislav.verinskij@gmail.com>
2022-11-28 22:24:22 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/filmslist/models/Extra.kt
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.filmslist.models
+
+data class Extra(
+    val explore_date: String,
+    val next_prelude_interval: Int
+)
---
+++ app/src/main/java/com/bignerdranch/android/filmslist/models/ImageModelList.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.filmslist.models
+
+data class ImageModelList(
+    val extra: Extra,
+    val photos: Photos,
+    val stat: String
+)
